c7e3d5a102139822d0cc6f78256fff3cb43737d9
Jim Nanney <jnanney@gmail.com>
2013-08-25 14:13:31 -0500

    fix failing integration spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "Static Pages" do
   describe "Home Page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'WHY RUBY ON SAILS'" do
       visit "/static_pages/home"
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('WHY RUBY ON SAILS')
     end
   end
 end
