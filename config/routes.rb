3e6c92bfa93f76bbc8674ce0305395171da81c62
Jim Nanney <jnanney@gmail.com>
2013-08-26 07:11:19 -0500

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Blog::Application.routes.draw do
 
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "about" => 'static_pages#about'
+  get "home" => 'static_pages#home'
+  get 'signup' => 'users#new'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,8 +3,15 @@ require 'spec_helper'
 describe "Static Pages" do
   describe "Home Page" do
     it "should have the content 'WHY RUBY ON SAILS'" do
-      visit "/static_pages/home"
+      visit "/home"
       expect(page).to have_content('WHY RUBY ON SAILS')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Ruby On Sails'" do
+      visit "/about"
+      expect(page).to have_content('About Ruby On Sails')
     end
   end
 end
